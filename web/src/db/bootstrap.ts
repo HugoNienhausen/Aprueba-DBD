3c5b0ff629a8fb0e8c7e970bebd4d10d79d085be
HugoNienhausen <hugo.nienhausen@gmail.com>
2026-04-04 19:15:08 +0200

    Corregir pregunta 137 + bump DATA_VERSION a 3

--- web/src/db/bootstrap.ts
+++ web/src/db/bootstrap.ts
@@ -16,7 +16,7 @@ const QUESTIONS_JSON_URL = "/data/questions_new.json";
  * Users with an older version will get a transparent re-import
  * without losing their test history (user_answers, test_sessions).
  */
-const DATA_VERSION = 2;
+const DATA_VERSION = 3;
 const DATA_VERSION_KEY = "data_version";
 
 export type BootstrapResult =
